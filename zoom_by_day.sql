a2591e8774ea4284b7f0f2711632bdaa6e69536d
muravlyansky <74048535+muravlyansky@users.noreply.github.com>
2020-11-13 00:22:13 +0300

    Статистика участия в Zoom v1.0

---
+++ zoom_by_day.sql
@@ -0,0 +1,21 @@
+WITH const AS 
+	  (SELECT   1 AS day_id, STR_TO_DATE("07 11 2020","%d %m %Y") AS day_n 	   UNION
+		SELECT   2 AS day_id, STR_TO_DATE("08 11 2020","%d %m %Y") AS day_n	   UNION
+		SELECT   3 AS day_id, STR_TO_DATE("09 11 2020","%d %m %Y") AS day_n	   UNION
+		SELECT   4 AS day_id, STR_TO_DATE("10 11 2020","%d %m %Y") AS day_n	   UNION
+		SELECT   5 AS day_id, STR_TO_DATE("11 11 2020","%d %m %Y") AS day_n	   UNION
+		SELECT   6 AS day_id, STR_TO_DATE("12 11 2020","%d %m %Y") AS day_n
+	  )
+SELECT zoom.user_name, zoom.email, zoom.day_n AS 'День', zoom.zoom_count
+FROM  const
+LEFT JOIN (
+				SELECT lmp.user_name, lmp.email, const.day_n, COUNT(lmp.meeting_id) AS zoom_count
+				FROM  prd_analytics.list_meetings_participants AS lmp  
+				JOIN const
+				WHERE lmp.join_time>=const.day_n AND lmp.join_time<const.day_n+1  AND
+						lmp.user_name NOT LIKE '___zal@a5000.ru%' AND lmp.email NOT LIKE '___zal@a5000.ru%'
+				GROUP BY lmp.user_name, const.day_n
+				HAVING COUNT(DISTINCT lmp.email)<=1
+			 ) AS zoom
+		ON zoom.day_n=const.day_n	;
+		
