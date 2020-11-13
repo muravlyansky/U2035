5a8be146c7f33a61b52ee23edaedc3ad7c34301e
muravlyansky <74048535+muravlyansky@users.noreply.github.com>
2020-11-13 12:45:29 +0300

    v.11 - добавлено 13.11

--- zoom_by_day.sql
+++ zoom_by_day.sql
@@ -1,15 +1,17 @@
+-- Собираем имена Zoom участников и e-mail + количество сессий  в день
 WITH const AS 
 	  (SELECT   1 AS day_id, STR_TO_DATE("07 11 2020","%d %m %Y") AS day_n 	   UNION
 		SELECT   2 AS day_id, STR_TO_DATE("08 11 2020","%d %m %Y") AS day_n	   UNION
 		SELECT   3 AS day_id, STR_TO_DATE("09 11 2020","%d %m %Y") AS day_n	   UNION
 		SELECT   4 AS day_id, STR_TO_DATE("10 11 2020","%d %m %Y") AS day_n	   UNION
 		SELECT   5 AS day_id, STR_TO_DATE("11 11 2020","%d %m %Y") AS day_n	   UNION
-		SELECT   6 AS day_id, STR_TO_DATE("12 11 2020","%d %m %Y") AS day_n
+		SELECT   6 AS day_id, STR_TO_DATE("12 11 2020","%d %m %Y") AS day_n		UNION
+		SELECT   7 AS day_id, STR_TO_DATE("13 11 2020","%d %m %Y") AS day_n			
 	  )
-SELECT zoom.user_name, zoom.email, zoom.day_n AS 'День', zoom.zoom_count
+SELECT user_name, email, const.day_n AS 'День' , zoom_count
 FROM  const
 LEFT JOIN (
-				SELECT lmp.user_name, lmp.email, const.day_n, COUNT(lmp.meeting_id) AS zoom_count
+				SELECT lmp.user_name, lmp.email, const.day_n , COUNT(lmp.meeting_id) AS zoom_count
 				FROM  prd_analytics.list_meetings_participants AS lmp  
 				JOIN const
 				WHERE lmp.join_time>=const.day_n AND lmp.join_time<const.day_n+1  AND
@@ -18,4 +20,3 @@ LEFT JOIN (
 				HAVING COUNT(DISTINCT lmp.email)<=1
 			 ) AS zoom
 		ON zoom.day_n=const.day_n	;
-		
